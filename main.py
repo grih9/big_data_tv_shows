68439484a1697ec62ec58b53e0156cf10d82c5ee
Grigoriy Tolstikov <tolstikov.gn@edu.spbstu.ru>
2022-10-10 22:21:47 +0300

    episodes

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from multiprocessing import Process
 if __name__ == '__main__':
     #scrap_shows(0)
 
-    p1 = Process(target=scrap_episodes, args=("episodes.csv", 42480, int(1 * NUMBER_OF_EPISODES / 6) - 1,))
-    p2 = Process(target=scrap_episodes, args=("episodes2.csv", int(1 * NUMBER_OF_EPISODES / 6) - 1, int(1 * NUMBER_OF_EPISODES / 3) - 1,))
-    p3 = Process(target=scrap_episodes, args=("episodes3.csv", 6228627, int(3 * NUMBER_OF_EPISODES / 6) - 1,))
-    p4 = Process(target=scrap_episodes, args=("episodes4.csv", int(3 * NUMBER_OF_EPISODES / 6) - 1, int(4 * NUMBER_OF_EPISODES / 6),))
+    p1 = Process(target=scrap_episodes, args=("episodes.csv", 42721, int(1 * NUMBER_OF_EPISODES / 24) - 1,))
+    p2 = Process(target=scrap_episodes, args=("episodes2.csv", int(1 * NUMBER_OF_EPISODES / 24) - 1, int(2 * NUMBER_OF_EPISODES / 24) - 1,))
+    p3 = Process(target=scrap_episodes, args=("episodes3.csv", int(2 * NUMBER_OF_EPISODES / 24) - 1, int(3 * NUMBER_OF_EPISODES / 24) - 1,))
+    #p3 = Process(target=scrap_episodes, args=("episodes3.csv", 6228627, int(3 * NUMBER_OF_EPISODES / 6) - 1,))
+    p4 = Process(target=scrap_episodes, args=("episodes4.csv", int(3 * NUMBER_OF_EPISODES / 24) - 1, int(4 * NUMBER_OF_EPISODES / 24),))
     #p4 = Process(target=scrap_episodes, args=("episodes4.csv", 12335049, int(3 * NUMBER_OF_EPISODES / 3),))
 
     p1.start()
